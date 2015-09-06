92d5cca81c469987dd8c65acad2cf6985fbc0a9b
Oleg Borodai <borodai.oleg@gmail.com>
2015-09-06 18:19:05 +0200

    looks ready for submit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # udacity-project-five
+To build the project just run 'grunt', results of build will appear in build folder
--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+'use strict';
+
+/*global ko, google, $*/
+
 var hamburg = {
     lat: 53.555,
     lng: 9.929
@@ -18,34 +22,22 @@ var ViewModel = function () {
         center: hamburg
     });
     this.service = new google.maps.places.PlacesService(this.map);
-
     this.locationList = ko.observableArray([]);
 
+    /*This function is executed after initial search of place through
+      google maps API and it populates observableArray with list of places*/
     this.addLocationCallback = function (results, status) {
         if (status == google.maps.places.PlacesServiceStatus.OK) {
             for (var i = 0; i < results.length; i++) {
-                /* marker is an object with additional data about the pin for
-                a single location */
                 var marker = new google.maps.Marker({
                     map: self.map,
                     position: results[i].geometry.location,
                     title: results[i].name
                 });
 
-                var popUpContent = self.getFsqReviews(results[i].name, marker);
+                self.getFsqReviews(results[i].name, marker);
 
-
-                (function (markerParam) {
-                    google.maps.event.addListener(markerParam, 'click', function () {
-                        self.closeInfoWindows();
-                        markerParam.myInfoWindow.open(self.map, markerParam);
-                        markerParam.setAnimation(google.maps.Animation.BOUNCE);
-                        window.setTimeout(function () {
-                            markerParam.setAnimation(null)
-                        }, 2000);
-                    });
-                })(marker);
-
+                self.addListenerForMarker(marker);
 
                 self.locationList.push(new Loc({
                     name: results[i].name,
@@ -55,16 +47,30 @@ var ViewModel = function () {
                 }));
             }
         }
+
+
     };
 
+    /*Trigger googlePlaces search to initially populate array with places*/
     self.service.textSearch({
         location: hamburg,
         radius: '1000',
         query: 'restaraunts'
     }, self.addLocationCallback);
 
-    //this.currentLocation = ko.observable(this.locationList()[0]);
+    /*service fucntion to add click listeneres on markers*/
+    this.addListenerForMarker = function (markerParam) {
+        google.maps.event.addListener(markerParam, 'click', function () {
+            self.closeInfoWindows();
+            markerParam.myInfoWindow.open(self.map, markerParam);
+            markerParam.setAnimation(google.maps.Animation.BOUNCE);
+            window.setTimeout(function () {
+                markerParam.setAnimation(null);
+            }, 2000);
+        });
+    };
 
+    /*service function to get reviews from foursquare*/
     this.getFsqReviews = function (locationName, marker) {
         var venueRequestUrl = 'https://api.foursquare.com/v2/venues/search?ll=53.55,9.93&client_id=QZHN3TDUICZBINB31CE3SZP3PYUZC5BLRYHZEFVYL0I51AU3&client_secret=U2IBTFX4XYW0TKFJI35U0JURW2YUVOWO1FX0COBHTTT3QK0Z&v=20150830&inten=match&limit=1&query=' + locationName;
         var tipRequestStartUrl = 'https://api.foursquare.com/v2/venues/';
@@ -72,8 +78,8 @@ var ViewModel = function () {
         var result = [];
         var html = '<h4>' + locationName + '</h4>';
         marker.myInfoWindow = new google.maps.InfoWindow({
-                            content: html
-                        });
+            content: html
+        });
         $.getJSON(venueRequestUrl, function (data) {
             if (data.meta.code == 200 && data.response.venues.length > 0) {
                 var venueId = data.response.venues[0].id;
@@ -87,7 +93,7 @@ var ViewModel = function () {
                                 user: tips[i].user.firstName + ' ' + tips[i].user.lastName
                             });
                         }
-                        for (var i = 0; i < result.length; i++) {
+                        for (i = 0; i < result.length; i++) {
                             html += '<p><strong>' + result[i].user + ':</strong>';
                             html += result[i].text + '</p>';
                         }
@@ -95,13 +101,13 @@ var ViewModel = function () {
                             content: html
                         });
                     }
-                }).error(function (e) {
-                    alert('Ooops, something went wrong');
+                }).error(function () {
+                    /*'Ooops, something went wrong'*/
                 });
 
             }
-        }).error(function (e) {
-            alert('Ooops, something went wrong');
+        }).error(function () {
+            /*'Ooops, something went wrong'*/
         });
     };
 
@@ -111,9 +117,8 @@ var ViewModel = function () {
         myMarker.myInfoWindow.open(self.map, myMarker);
         myMarker.setAnimation(google.maps.Animation.BOUNCE);
         self.map.setCenter(myMarker.getPosition());
-        //self.currentLocation(this);
         window.setTimeout(function () {
-            myMarker.setAnimation(null)
+            myMarker.setAnimation(null);
         }, 2000);
     };
 
@@ -131,20 +136,18 @@ var ViewModel = function () {
     };
 
     this.searchLocation = function (formElement) {
-        var name = "", desc = "", reviews = "",
+        var name = '',
+            desc = '',
             searchTerm = formElement.searchText.value.toLowerCase(),
             myLocations = self.locationList.removeAll();
 
         self.closeInfoWindows();
         self.clearLocations();
 
-        for(var i = 0; i < myLocations.length; i++) {
+        for (var i = 0; i < myLocations.length; i++) {
             name = myLocations[i].name().toLowerCase();
             desc = myLocations[i].description().toLowerCase();
-            reviews = myLocations[i].marker().myInfoWindow.content.toLowerCase();
-            if(name.search(searchTerm) >= 0 ||
-               desc.search(searchTerm) >= 0 ||
-               reviews.search(searchTerm) >= 0) {
+            if (name.search(searchTerm) != -1 || desc.search(searchTerm) != -1) {
                 myLocations[i].visible(true);
                 myLocations[i].marker().setMap(self.map);
             } else {
@@ -152,9 +155,11 @@ var ViewModel = function () {
                 myLocations[i].marker().setMap(null);
             }
             self.locationList.push(myLocations[i]);
-
-            console.log("search loop");
         }
+        var results = self.locationList().filter(function (elem) {
+            return elem.visible() === true;
+        });
+        self.map.setCenter(results[0].marker().getPosition());
     };
 };
 
